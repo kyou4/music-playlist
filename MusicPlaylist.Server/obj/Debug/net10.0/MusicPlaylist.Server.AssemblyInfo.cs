f59d945d34374073713eb251dde5e05f7122a461
Kyle You <kyou4@my.bcit.ca>
2026-04-09 17:07:00 -0700

    fi workflow

--- MusicPlaylist.Server/obj/Debug/net10.0/MusicPlaylist.Server.AssemblyInfo.cs
+++ MusicPlaylist.Server/obj/Debug/net10.0/MusicPlaylist.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicPlaylist.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71f8aae0c8524576ea12e504a1dacc84bb32ced4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6706d6041c8b67a38eee98003087f499cd5daec9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicPlaylist.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicPlaylist.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
